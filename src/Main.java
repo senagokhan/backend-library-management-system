de202479c025e887cff584d8ad9907f89df23d3b
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-06-04 20:21:05 +0300

    LMS-init

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    static int Index = 50;
+    static String[][] books = new String[Index][4];
+    static String[][] users = new String[Index][4];
+    static String[][] transactions = new String[Index][3];
+    static int bookQuantity = 0;
+    static int transactionQuantity = 0;
+    static int userQuantity = 0;
+
+
+    public static void main(String[] args) {
+
+    }
+}
