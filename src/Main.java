0341280ce4523c6db298305d59b0e52c3784f1b5
nihansulukoyozen <nihansulukoy@gmail.com>
2024-06-24 16:33:04 +0300

    search book

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,84 @@
-import java.util.Scanner;
-
 public class Main {
     static int Index = 50;
-    static String[][] books = new String[Index][4];
+    static String[][] books = new String[Index][4]; // title , author , id , additional doc.
     static String[][] users = new String[Index][4];
-    static String[][] transactions = new String[Index][3];
+    static String[][] transactions = new String[Index][3]; // userId , bookId , date , status
     static int bookQuantity = 0;
     static int transactionQuantity = 0;
     static int userQuantity = 0;
 
 
     public static void main(String[] args) {
-        int i ;
-        String bookName;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter the name of the book you want to search");
-        bookName = scan.nextLine();
-
-        for(i=0 ; i<=Index ; i++){
-
-            if(bookName.equals("books[i][0]")){
-                System.out.println("Book Name = books[i][0]");
-                System.out.println("ISBN = books[i][1]");
-                System.out.println("Author = books[i][2]");
-                System.out.println("Number of Pages = books[i][3]");
-            }
-
-
-            else{}
-
-        }
-                System.out.println("The book you are looking for is not registered in the system:");
-
     }
 
+    public static void addBook(String title, String author, String bookId, String additionalDoc) {
+        if (bookQuantity < Index) {
+            books[bookQuantity][0] = title;
+            books[bookQuantity][1] = author;
+            books[bookQuantity][2] = bookId;
+            books[bookQuantity][3] = additionalDoc;
+            bookQuantity++;
 
+            System.out.println(bookQuantity + "." + " Book added successfully");
+        } else {
+            System.out.println("We cannot add books to shelves that are already full!");
 
+        }
+    }
+
+    public static void viewAvailableBooks() {
+
+        if (bookQuantity == 0) {
+            System.out.println("There isn't available books!");
+        } else {
+            System.out.println("Available books:");
+            for (int i = 0; i < bookQuantity; i++) {
+                printBooks(books[i][0], books[i][1], books[i][2], books[i][3]);
+            }
+        }
+    }
+
+    private static void printBooks(String title, String author, String bookId, String additionalDoc) {
+        System.out.println("Book Title: " + title);
+        System.out.println("Book Author: " + author);
+        System.out.println("Book ID: " + bookId);
+        System.out.println("Book Additional Document: " + additionalDoc);
+    }
+
+    //Total number of books
+    public static void countTotalBooks() {
+        System.out.println("Total number of books :" + bookQuantity);
+    }
+
+    static void checkBooks(String bookId) {
+        boolean found = false;
+        for (int i = 0; i < bookQuantity; i++) {
+            if (books[i][2].equals(bookId)) {
+                System.out.println("The book is found!");
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("The book is not found.");
+        }
+    }
+
+    public static void searchBook(String query) { // Search with title or Id
+        int temp = -1;
+        for (int i = 0; i < bookQuantity; i++) {
+            if (books[i][0].equals(query) || books[i][2].equals(query)) {
+                System.out.println("The book was found :");
+                System.out.println("--Book Information--");
+                System.out.println("Book title : " + books[i][0]);
+                System.out.println("Book Author : " + books[i][1]);
+                System.out.println("Book ID : " + books[i][2]);
+                System.out.println("Book Additional Document : " + books[i][3]);
+                temp = i;
+            }
+        }
+        if (temp == -1) {
+            System.out.println("The book is not found : ");
+        }
+    }
 }
